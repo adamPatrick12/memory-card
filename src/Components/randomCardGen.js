1e23ebe6b7d7ddcbe86eb7ec24beb66b29db132e
Adam Patrick <adam_77paper@hotmail.com>
2021-12-26 19:23:16 -0500

    Fully implemented game logic

--- src/Components/randomCardGen.js
+++ src/Components/randomCardGen.js
@@ -5,6 +5,20 @@ let scoreNum = 0;
 let bestScore = 0
 let cardClicked1 = 0;
 let cardClicked2 = 0;
+let cardClicked3 = 0;
+let cardClicked4 = 0;
+let cardClicked5 = 0;
+let cardClicked6 = 0;
+let cardClicked7 = 0;
+let cardClicked8 = 0;
+let cardClicked9 = 0;
+let cardClicked10 = 0;
+let cardClicked11 = 0;
+let cardClicked12 = 0;
+let cardClicked13 = 0;
+let cardClicked14 = 0;
+let cardClicked15 = 0;
+
 
 const Randomizor = () => {
   const shuffle = () => {
@@ -19,31 +33,219 @@ const Randomizor = () => {
     update([...cards]);
   };
 
-  const checkIfClicked = (e) => {
-    let textContent = e.target.textContent;
-    
-
-    if (textContent === "1") {
-      cardClicked1++;
-      scoreNum++
-      updateScore(scoreNum)
-      if (cardClicked1 === 2) {
-        bestScore = scoreNum-1
-        updateHighScore(bestScore)
-        scoreNum = 0
-        updateScore(scoreNum)
-        cardClicked1 = 0
+        //Alot is going on here so let me explain 
+  const checkIfClicked = (e) => {       
+    let textContent = e.target.textContent;   //store the textcontent in a variable to check what card the user clicked
+  
+    if (textContent === "1") {    //Checking what card the user clicked
+      cardClicked1++;     //Adding +1 to a variable so the program knows the user alrealdy clicked that card
+      scoreNum++          // If it is the first time clicking the score will increase by one
+      updateScore(scoreNum) //updating state
+      if (cardClicked1 === 2) {   //If card is clicked again program will enter into this conditional 
+        if(bestScore < scoreNum-1){   //checking if the bestscore is less then current score, if it is then we update the best score
+          bestScore = scoreNum-1      
+          updateHighScore(bestScore)
+        }
+        scoreNum = 0            //score is set to 0 since user clicked same card twice
+        updateScore(scoreNum)   //updating state
+        cardClicked1 = cardClicked2  = cardClicked3 = cardClicked4 = cardClicked5 = cardClicked6 = cardClicked7 = 
+        cardClicked8 = cardClicked9 = cardClicked10 = cardClicked11 = cardClicked12 = cardClicked13 = cardClicked14 = cardClicked15 = 0         //set card click back to one so user can play again right away
       }
     } else if (textContent === "2") {
       scoreNum++
       cardClicked2++;
       updateScore(scoreNum)
       if (cardClicked2 === 2) {
-        bestScore = scoreNum-1
-        updateHighScore(bestScore)
+        if(bestScore < scoreNum-1){
+          bestScore = scoreNum-1
+          updateHighScore(bestScore)
+        }
         scoreNum = 0
         updateScore(scoreNum)
-        cardClicked2 = 0
+        cardClicked1 = cardClicked2  = cardClicked3 = cardClicked4 = cardClicked5 = cardClicked6 = cardClicked7 = 
+        cardClicked8 = cardClicked9 = cardClicked10 = cardClicked11 = cardClicked12 = cardClicked13 = cardClicked14 = cardClicked15 = 0   
+      }
+    }else if (textContent === "3") {
+      scoreNum++
+      cardClicked3++;
+      updateScore(scoreNum)
+      if (cardClicked3 === 2) {
+        if(bestScore < scoreNum-1){
+          bestScore = scoreNum-1
+          updateHighScore(bestScore)
+        }
+        scoreNum = 0
+        updateScore(scoreNum)
+        cardClicked1 = cardClicked2  = cardClicked3 = cardClicked4 = cardClicked5 = cardClicked6 = cardClicked7 = 
+        cardClicked8 = cardClicked9 = cardClicked10 = cardClicked11 = cardClicked12 = cardClicked13 = cardClicked14 = cardClicked15 = 0 
+      }
+    }else if (textContent === "4") {
+      scoreNum++
+      cardClicked4++;
+      updateScore(scoreNum)
+      if (cardClicked4 === 2) {
+        if(bestScore < scoreNum-1){
+          bestScore = scoreNum-1
+          updateHighScore(bestScore)
+        }
+        scoreNum = 0
+        updateScore(scoreNum)
+        cardClicked1 = cardClicked2  = cardClicked3 = cardClicked4 = cardClicked5 = cardClicked6 = cardClicked7 = 
+        cardClicked8 = cardClicked9 = cardClicked10 = cardClicked11 = cardClicked12 = cardClicked13 = cardClicked14 = cardClicked15 = 0 
+      }
+    } else if (textContent === "5") {
+      scoreNum++
+      cardClicked5++;
+      updateScore(scoreNum)
+      if (cardClicked5 === 2) {
+        if(bestScore < scoreNum-1){
+          bestScore = scoreNum-1
+          updateHighScore(bestScore)
+        }
+        scoreNum = 0
+        updateScore(scoreNum)
+        cardClicked1 = cardClicked2  = cardClicked3 = cardClicked4 = cardClicked5 = cardClicked6 = cardClicked7 = 
+        cardClicked8 = cardClicked9 = cardClicked10 = cardClicked11 = cardClicked12 = cardClicked13 = cardClicked14 = cardClicked15 = 0 
+      }
+    }else if (textContent === "6") {
+      scoreNum++
+      cardClicked6++;
+      updateScore(scoreNum)
+      if (cardClicked6 === 2) {
+        if(bestScore < scoreNum-1){
+          bestScore = scoreNum-1
+          updateHighScore(bestScore)
+        }
+        scoreNum = 0
+        updateScore(scoreNum)
+        cardClicked1 = cardClicked2  = cardClicked3 = cardClicked4 = cardClicked5 = cardClicked6 = cardClicked7 = 
+        cardClicked8 = cardClicked9 = cardClicked10 = cardClicked11 = cardClicked12 = cardClicked13 = cardClicked14 = cardClicked15 = 0 
+      }
+    }else if (textContent === "7") {
+      scoreNum++
+      cardClicked7++;
+      updateScore(scoreNum)
+      if (cardClicked7 === 2) {
+        if(bestScore < scoreNum-1){
+          bestScore = scoreNum-1
+          updateHighScore(bestScore)
+        }
+        scoreNum = 0
+        updateScore(scoreNum)
+        cardClicked1 = cardClicked2  = cardClicked3 = cardClicked4 = cardClicked5 = cardClicked6 = cardClicked7 = 
+        cardClicked8 = cardClicked9 = cardClicked10 = cardClicked11 = cardClicked12 = cardClicked13 = cardClicked14 = cardClicked15 = 0 
+      }
+    }else if (textContent === "8") {
+      scoreNum++
+      cardClicked8++;
+      updateScore(scoreNum)
+      if (cardClicked8 === 2) {
+        if(bestScore < scoreNum-1){
+          bestScore = scoreNum-1
+          updateHighScore(bestScore)
+        }
+        scoreNum = 0
+        updateScore(scoreNum)
+        cardClicked1 = cardClicked2  = cardClicked3 = cardClicked4 = cardClicked5 = cardClicked6 = cardClicked7 = 
+        cardClicked8 = cardClicked9 = cardClicked10 = cardClicked11 = cardClicked12 = cardClicked13 = cardClicked14 = cardClicked15 = 0 
+      }
+    }else if (textContent === "9") {
+      scoreNum++
+      cardClicked9++;
+      updateScore(scoreNum)
+      if (cardClicked9 === 2) {
+        if(bestScore < scoreNum-1){
+          bestScore = scoreNum-1
+          updateHighScore(bestScore)
+        }
+        scoreNum = 0
+        updateScore(scoreNum)
+        cardClicked1 = cardClicked2  = cardClicked3 = cardClicked4 = cardClicked5 = cardClicked6 = cardClicked7 = 
+        cardClicked8 = cardClicked9 = cardClicked10 = cardClicked11 = cardClicked12 = cardClicked13 = cardClicked14 = cardClicked15 = 0 
+      }
+    }else if (textContent === "10") {
+      scoreNum++
+      cardClicked10++;
+      updateScore(scoreNum)
+      if (cardClicked10 === 2) {
+        if(bestScore < scoreNum-1){
+          bestScore = scoreNum-1
+          updateHighScore(bestScore)
+        }
+        scoreNum = 0
+        updateScore(scoreNum)
+        cardClicked1 = cardClicked2  = cardClicked3 = cardClicked4 = cardClicked5 = cardClicked6 = cardClicked7 = 
+        cardClicked8 = cardClicked9 = cardClicked10 = cardClicked11 = cardClicked12 = cardClicked13 = cardClicked14 = cardClicked15 = 0 
+      }
+    }else if (textContent === "11") {
+      scoreNum++
+      cardClicked11++;
+      updateScore(scoreNum)
+      if (cardClicked11 === 2) {
+        if(bestScore < scoreNum-1){
+          bestScore = scoreNum-1
+          updateHighScore(bestScore)
+        }
+        scoreNum = 0
+        updateScore(scoreNum)
+        cardClicked1 = cardClicked2  = cardClicked3 = cardClicked4 = cardClicked5 = cardClicked6 = cardClicked7 = 
+        cardClicked8 = cardClicked9 = cardClicked10 = cardClicked11 = cardClicked12 = cardClicked13 = cardClicked14 = cardClicked15 = 0 
+      }
+    }else if (textContent === "12") {
+      scoreNum++
+      cardClicked12++;
+      updateScore(scoreNum)
+      if (cardClicked12 === 2) {
+        if(bestScore < scoreNum-1){
+          bestScore = scoreNum-1
+          updateHighScore(bestScore)
+        }
+        scoreNum = 0
+        updateScore(scoreNum)
+        cardClicked1 = cardClicked2  = cardClicked3 = cardClicked4 = cardClicked5 = cardClicked6 = cardClicked7 = 
+        cardClicked8 = cardClicked9 = cardClicked10 = cardClicked11 = cardClicked12 = cardClicked13 = cardClicked14 = cardClicked15 = 0 
+      }
+    }else if (textContent === "13") {
+      scoreNum++
+      cardClicked13++;
+      updateScore(scoreNum)
+      if (cardClicked13 === 2) {
+        if(bestScore < scoreNum-1){
+          bestScore = scoreNum-1
+          updateHighScore(bestScore)
+        }
+        scoreNum = 0
+        updateScore(scoreNum)
+        cardClicked1 = cardClicked2  = cardClicked3 = cardClicked4 = cardClicked5 = cardClicked6 = cardClicked7 = 
+        cardClicked8 = cardClicked9 = cardClicked10 = cardClicked11 = cardClicked12 = cardClicked13 = cardClicked14 = cardClicked15 = 0 
+      }
+    }else if (textContent === "14") {
+      scoreNum++
+      cardClicked14++;
+      updateScore(scoreNum)
+      if (cardClicked14 === 2) {
+        if(bestScore < scoreNum-1){
+          bestScore = scoreNum-1
+          updateHighScore(bestScore)
+        }
+        scoreNum = 0
+        updateScore(scoreNum)
+        cardClicked1 = cardClicked2  = cardClicked3 = cardClicked4 = cardClicked5 = cardClicked6 = cardClicked7 = 
+        cardClicked8 = cardClicked9 = cardClicked10 = cardClicked11 = cardClicked12 = cardClicked13 = cardClicked14 = cardClicked15 = 0 
+      }
+    }else if (textContent === "15") {
+      scoreNum++
+      cardClicked15++;
+      updateScore(scoreNum)
+      if (cardClicked15 === 2) {
+        if(bestScore < scoreNum-1){
+          bestScore = scoreNum-1
+          updateHighScore(bestScore)
+        }
+        scoreNum = 0
+        updateScore(scoreNum)
+        cardClicked1 = cardClicked2  = cardClicked3 = cardClicked4 = cardClicked5 = cardClicked6 = cardClicked7 = 
+        cardClicked8 = cardClicked9 = cardClicked10 = cardClicked11 = cardClicked12 = cardClicked13 = cardClicked14 = cardClicked15 = 0 
       }
     }
   };
@@ -64,67 +266,67 @@ const Randomizor = () => {
     </div>
   );
   const element3 = (
-    <div className="gamePiece" onClick={shuffle}>
+    <div className="gamePiece" onClick={onClick}>
       3
     </div>
   );
   const element4 = (
-    <div className="gamePiece" onClick={shuffle}>
+    <div className="gamePiece" onClick={onClick}>
       4
     </div>
   );
   const element5 = (
-    <div className="gamePiece" onClick={shuffle}>
+    <div className="gamePiece" onClick={onClick}>
       5
     </div>
   );
   const element6 = (
-    <div className="gamePiece" onClick={shuffle}>
+    <div className="gamePiece" onClick={onClick}>
       6
     </div>
   );
   const element7 = (
-    <div className="gamePiece" onClick={shuffle}>
+    <div className="gamePiece" onClick={onClick}>
       7
     </div>
   );
   const element8 = (
-    <div className="gamePiece" onClick={shuffle}>
+    <div className="gamePiece" onClick={onClick}>
       8
     </div>
   );
   const element9 = (
-    <div className="gamePiece" onClick={shuffle}>
+    <div className="gamePiece" onClick={onClick}>
       9
     </div>
   );
   const element10 = (
-    <div className="gamePiece" onClick={shuffle}>
+    <div className="gamePiece" onClick={onClick}>
       10
     </div>
   );
   const element11 = (
-    <div className="gamePiece" onClick={shuffle}>
+    <div className="gamePiece" onClick={onClick}>
       11
     </div>
   );
   const element12 = (
-    <div className="gamePiece" onClick={shuffle}>
+    <div className="gamePiece" onClick={onClick}>
       12
     </div>
   );
   const element13 = (
-    <div className="gamePiece" onClick={shuffle}>
+    <div className="gamePiece" onClick={onClick}>
       13
     </div>
   );
   const element14 = (
-    <div className="gamePiece" onClick={shuffle}>
+    <div className="gamePiece" onClick={onClick}>
       14
     </div>
   );
   const element15 = (
-    <div className="gamePiece" onClick={shuffle}>
+    <div className="gamePiece" onClick={onClick}>
       15
     </div>
   );
@@ -157,9 +359,9 @@ const Randomizor = () => {
 
   return (
   <div>
+  <div className = "score" > Score: {score}</div>
+  <div className = "score">Highscore: {highScore}</div>
   <div>{cards}</div>
-  <div>{score}</div>
-  <div>{highScore}</div>
   </div>
   );
 };
